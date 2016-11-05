02c167057f53d1fd7c7c54d9d0d98ef8601ad00c
blueplanet <erguolinge@gmail.com>
2016-11-05 10:48:33 +0900

    customerを保存しておく

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'config'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'binding_of_caller'
   gem 'rspec-rails'
 end
 
--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,21 +4,26 @@ class ChargesController < ApplicationController
   end
 
   def create
-    @charge = current_user.charges.build charge_params
-    
-    customer = Stripe::Customer.create(
-      email: current_user.email,
-      source: params[:stripeToken]
-    )
+    current_user.transaction do
+      @charge = current_user.charges.build charge_params
 
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: @charge.amount,
-      description: 'Rails stripe test',
-      currency: 'jpy'
-    )
-    @charge.data = charge.to_json
-    @charge.save!
+      if current_user.stripe_customer_token.blank?
+        customer = Stripe::Customer.create(
+          email: current_user.email,
+          source: params[:stripeToken]
+        )
+        current_user.update stripe_customer_token: customer.id
+      end
+
+      charge = Stripe::Charge.create(
+        customer: current_user.stripe_customer_token,
+        amount: @charge.amount,
+        description: 'Rails stripe test',
+        currency: 'jpy'
+      )
+      @charge.data = charge.to_json
+      @charge.save!
+    end
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
