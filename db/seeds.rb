2693d18cb8894b1f6df8ad4553236ef5ba603e00
blueplanet <erguolinge@gmail.com>
2016-11-06 18:11:51 +0900

    plan rake追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Plan.create(
+  id: 1,
+  stripe_plan_id: 'year',
+  name: '年払い',
+  currency: :jpy,
+  interval: 'year',
+  amount: 10_000
+)
+
+Plan.create(
+  id: 2,
+  stripe_plan_id: 'month',
+  name: '月払い',
+  currency: :jpy,
+  interval: 'month',
+  amount: 1_000
+)
+
+Plan.find_each do |plan|
+  Stripe::Plan.create(
+    id: plan.stripe_plan_id,
+    amount: plan.amount,
+    currency: plan.currency,
+    interval: plan.interval,
+    name: plan.name
+  )
+end
