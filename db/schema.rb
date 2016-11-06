72aa17903a1eda08f71329d80a1337ca48443f68
blueplanet <erguolinge@gmail.com>
2016-11-06 18:31:56 +0900

    プランを表示する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106082950) do
+ActiveRecord::Schema.define(version: 20161106092731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,9 +52,12 @@ ActiveRecord::Schema.define(version: 20161106082950) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "stripe_customer_token"
+    t.integer  "plan_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["plan_id"], name: "index_users_on_plan_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "charges", "users"
+  add_foreign_key "users", "plans"
 end
