9c540eae5de2fe48f08f0637c0320d0b11375df8
blueplanet <erguolinge@gmail.com>
2016-11-06 21:35:13 +0900

    customer作成処理追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,27 @@
 class UsersController < ApplicationController
   def update
-    if current_user.stripe_customer_token.blank?
-      stripe_customer = Stripe::Customer.create(
-        email: current_user.email,
-        plan: Plan.find(params[:user][:plan_id]).stripe_plan_id,
-        source: params[:stripeToken]
-      )
+    current_user.transaction do
+      if current_user.stripe_customer_token.blank?
+        stripe_customer = Stripe::Customer.create(
+          email: current_user.email,
+          plan: Plan.find(params[:user][:plan_id]).stripe_plan_id,
+          source: params[:stripeToken]
+        )
 
-      current_user.update stripe_customer_token: stripe_customer.id
+        current_user.update stripe_customer_token: stripe_customer.id
+      end
+
+      
+
+      current_user.update user_params
     end
 
     redirect_to new_charge_path
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:plan_id)
+    end
 end
