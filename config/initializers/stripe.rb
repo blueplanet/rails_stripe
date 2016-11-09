8d4d56de04b2f9acb69585d9b4fe9bfbaa5942ac
blueplanet <erguolinge@gmail.com>
2016-11-10 06:14:57 +0900

    Add event proc

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,18 @@
 Stripe.api_key = Settings.stripe.secret_key
+
+StripeEvent.configure do |events|
+  events.subscribe 'charge.failed' do |event|
+    # Define subscriber behavior based on the event object
+    event.class       #=> Stripe::Event
+    event.type        #=> "charge.failed"
+    event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
+  end
+
+  events.all do |event|
+    # Handle all event types - logging, etc.
+
+    Rails.logger.debug "********** Stripe Events start **********"
+    Rails.logger.debug event
+    Rails.logger.debug "********** Stripe Events end **********"
+  end
+end
